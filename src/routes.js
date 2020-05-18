90d32cfc0a908c75b0b716bb85a170428718449b
Pranav Karawale <pdkarawale31@gmail.com>
2020-05-18 15:55:25 +0530

    chore: rename index.md's to README.md's

--- src/routes.js
+++ src/routes.js
@@ -26,7 +26,7 @@ export default [
     path: "/docs",
     title: "TeddyTags Docs",
     render: async () => {
-      const res = await fetch(`${DOCS_PATH}/index.md`);
+      const res = await fetch(`${DOCS_PATH}/README.md`);
       const md = await res.text();
       return <DocsOrBlog text={md} />;
     },
@@ -35,10 +35,11 @@ export default [
     path: "/docs/:endOfPath",
     title: "TeddyTags Docs",
     render: async (p) => {
+      p.endOfPath.replace(/\/$/g, "");
       const endPoints = ["api", "/", ""];
       let fetchPath = `${DOCS_PATH}/${p.endOfPath}`;
       if (endPoints.some((bp) => bp === p.endOfPath))
-        fetchPath = `${DOCS_PATH}/${p.endOfPath}/index`;
+        fetchPath = `${DOCS_PATH}/${p.endOfPath}/README`;
       const md = await fetchMD(fetchPath);
       return <DocsOrBlog text={md} />;
     },
@@ -47,7 +48,7 @@ export default [
     path: "/blog",
     title: "TeddyTags Blog",
     render: async () => {
-      const res = await fetch(`${BLOG_PATH}/index.md`);
+      const res = await fetch(`${BLOG_PATH}/README.md`);
       const md = await res.text();
       return <DocsOrBlog activeLink="blog" text={md} />;
     },
